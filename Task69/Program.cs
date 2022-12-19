6f4870c3d5051bc8f6cd34e4b2ce5279e352d670
Malibu <malibu090@mail.ru>
2022-12-19 23:00:15 +0300

    A в степень B рекурсией

---
+++ Task69/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+int EnterNumber(string message)
+{
+    System.Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int OutputPower(int number, int power)
+{
+    if (power == 1) return number;
+    return (number * OutputPower(number, power - 1));
+}
+// Вывод натуральных чисел от M до N
+
+int number = EnterNumber("Input number: ");
+int power = EnterNumber("Input power: ");
+Console.WriteLine(OutputPower(number, power));
