7e09f8feb5420c9025aa583fe3e91d664ae4208c
Malibu <malibu090@mail.ru>
2022-12-19 22:59:22 +0300

    Сумма цифр числа рекурсией

---
+++ Task67/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int EnterNumber(string message)
+{
+    System.Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int OutputSumOfDigits(int number)
+{
+    if (number < 10)
+    {
+        return number;
+    }
+    return number % 10 + OutputSumOfDigits(number / 10);
+}
+// Вывод натуральных чисел от  до N
+
+int number = EnterNumber("Input number: ");
+Console.WriteLine(OutputSumOfDigits(number));
